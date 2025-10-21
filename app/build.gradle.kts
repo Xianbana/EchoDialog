1095667b8fbb56d6ffe79e6f86206b623eff1ad5
xianban <1196525229@qq.com>
2025-10-21 16:45:48 +0800

    优化依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,4 @@ dependencies {
     implementation(project(":library"))
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-
-
 }
